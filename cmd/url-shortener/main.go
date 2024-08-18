5628c688a6670ac17cbb66f5cb7cb996a6e2ca1d
maxgotech <golovinmd@mail.ru>
2024-08-18 20:33:29 +0300

    feat: init .env before main

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"url-shortener/internal/config"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+
+	fmt.Print(cfg)
 
 	// TODO: init logger: slog
 
