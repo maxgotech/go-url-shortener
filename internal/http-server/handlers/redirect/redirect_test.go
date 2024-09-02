8e00f77e9376f7eaae2debdde3adfa088c6272d9
maxgotech <golovinmd@mail.ru>
2024-09-02 23:58:08 +0300

    test: updated redirect tests

--- internal/http-server/handlers/redirect/redirect_test.go
+++ internal/http-server/handlers/redirect/redirect_test.go
@@ -1,31 +1,50 @@
 package redirect_test
 
 import (
+	"errors"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/redirect/mocks"
-	"url-shortener/internal/lib/api"
 	"url-shortener/internal/lib/logger/handlers/slogdiscard"
+	"url-shortener/internal/storage"
 )
 
-func TestSaveHandler(t *testing.T) {
+func TestRedirectHandler(t *testing.T) {
 	cases := []struct {
 		name      string
 		alias     string
 		url       string
 		respError string
 		mockError error
+		code      int
 	}{
 		{
 			name:  "Success",
 			alias: "test_alias",
 			url:   "https://www.github.com",
+			code:  http.StatusFound,
+		},
+		{
+			name:      "Error not found",
+			alias:     "test",
+			url:       "https://www.github.com",
+			respError: "not found",
+			mockError: storage.ErrURLNotFound,
+			code:      http.StatusNotFound,
+		},
+		{
+			name:      "unexpected error",
+			alias:     "alias",
+			respError: "failed to delete Alias",
+			mockError: errors.New("unexpected error"),
+			code:      http.StatusInternalServerError,
 		},
 	}
 
@@ -38,17 +57,20 @@ func TestSaveHandler(t *testing.T) {
 					Return(tc.url, tc.mockError).Once()
 			}
 
-			r := chi.NewRouter()
-			r.Get("/{alias}", redirect.New(slogdiscard.NewDiscardLogger(), urlGetterMock))
+			handler := chi.NewRouter()
+			handler.Get("/{alias}", redirect.New(slogdiscard.NewDiscardLogger(), urlGetterMock))
 
-			ts := httptest.NewServer(r)
-			defer ts.Close()
-
-			redirectedToURL, err := api.GetRedirect(ts.URL + "/" + tc.alias)
+			req, err := http.NewRequest(http.MethodGet, "/"+tc.alias, nil)
 			require.NoError(t, err)
 
-			// Check the final URL after redirection.
-			assert.Equal(t, tc.url, redirectedToURL)
+			rr := httptest.NewRecorder()
+			handler.ServeHTTP(rr, req)
+
+			require.Equal(t, rr.Code, tc.code)
+
+			if tc.code == http.StatusFound {
+				require.Equal(t, rr.Header().Get("Location"), tc.url)
+			}
 		})
 	}
 }
