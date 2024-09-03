931ebe9c3c6eecd9912befc740fa24d43af8d61c
maxgotech <golovinmd@mail.ru>
2024-09-03 14:19:15 +0300

    feat: basic auth

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -73,9 +73,17 @@ func main() {
 	// req parser
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.New(log, storage, cfg.AliasLength))
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(log, storage, cfg.HTTPServer.AliasLength))
+
+		r.Delete("/{alias}", delete.New(log, storage))
+	})
+
 	router.Get("/{alias}", redirect.New(log, storage))
-	router.Delete("/{alias}", delete.New(log, storage))
 	
 	log.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -12,13 +12,15 @@ type Config struct {
 	Env         string     `yaml:"env" env-default:"local" env-required:"true"`
 	StoragePath string     `yaml:"storage_path" env-required:"true"`
 	HTTPServer  HTTPServer `yaml:"http_server"`
-	AliasLength int        `yaml:"alias_length" env-default:"5" env-required:"true"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-required:"true"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	User        string        `yaml:"user" env-required:"true"`
+	AliasLength int           `yaml:"alias_length" env-default:"5" env-required:"true"`
+	Password    string        `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 
 func MustLoad() *Config {
